50d604a9eb7aee16cc607d1638a65ba6d1570841
ArjunRamesh <arjunramesh2001@gmail.com>
2023-11-29 16:40:14 +0530

    First commit for IntelliChat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Chatbot from 'react-simple-chatbot';
+import {Segment} from 'semantic-ui-react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const steps = [
+    {
+      id:'Greetings!',
+      message:'Hello I am Telexa',
+      trigger:'Ask Name'
+    },
+  {
+  id: 'Ask Name',
+  message: 'Please enter your name',
+  trigger: 'Waiting1'
+},
+{
+  id: 'Waiting1', 
+  user: true,
+  trigger: 'Name'
+},
+    {
+      id: 'Name',
+      message: "Hi {previousValue}. Please select your industry" ,
+      trigger: 'Industry'
+    },
+    
+    {
+      id:'Industry',
+      options:[ {value:'Information Technology',label:'Information Technology' , trigger:'IT'},
+      {value:'Human Resources',label:'Human Resources' , trigger:'HR'},
+      ],
+    },
+    {
+      id:'IT',
+      message: 'Thanks for choosing IT industry',
+      end: true
+    },
+    {
+      id:'HR',
+      message: 'Thanks for choosing HR industry',
+      end: true
+    }
+  ]
+  return(
+  <>
+  <Segment floated="right">
+    <Chatbot steps={steps}/>
+
+  </Segment>
+  
+  
+  </>
+);
 }
 
-export default App;
+export default App
